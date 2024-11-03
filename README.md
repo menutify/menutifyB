50cd7aa5ca1afe4462c342592c7b48c1716fd133
elkake <gianco.marquez@gmail.com>
2024-11-03 08:42:46 -0300

    cambios de repo

---
+++ README.md
@@ -0,0 +1,15 @@
+production
+- main
+- feature (new)
+- hotfix (problems)
+- enhancement (optim.)
+
+status code:
+- 400  bac request
+- 404 no existe
+- 401 dato incorrecto
+- 500 error
+- 200 ok
+- 201 created
+- 204 nocontent
+- 304 no modified
--- src/helper/mailerConfig.js
+++ src/helper/mailerConfig.js
@@ -1,6 +1,15 @@
 import nodemailer from 'nodemailer'
 
 // Configura el transporte de nodemailer con tu cuenta de Gmail
+// export const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+
+//   auth: {
+//     user: 'gianco.marquez@gmail.com',
+//     pass: 'vpps fgpy bzrv aixb' // Es mejor usar variables de entorno para seguridad
+//   }
+// })
+
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
 
