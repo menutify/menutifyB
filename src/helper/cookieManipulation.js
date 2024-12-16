45d879baafe58ba09a9c8e30d0119bcbbdd3b844
elkake <gianco.marquez@gmail.com>
2024-12-16 02:10:46 -0500

    cookeis para prod

--- src/helper/cookieManipulation.js
+++ src/helper/cookieManipulation.js
@@ -1,11 +1,24 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const setTokenToCookies = (res, content, name = 'authToken') => {
   // enviar el token por res
   res.cookie(name, content, {
     httpOnly: true,
-    secure: false, // Cambia a true si estás usando HTTPS
+    secure: true, // Cambia a true si estás usando HTTPS,
+    sameSite: 'None', // Es necesario para permitir cookies en sitios cruzados
     maxAge: 24 * 60 * 60 * 1000, // 4 horas en milisegundos
+    domain: process.env.FRONT_PATH,
     signed: true
   })
+
+  // res.cookie(name, content, {
+  //   httpOnly: true,
+  //   secure: false, // Cambia a true si estás usando HTTPS
+  //   maxAge: 24 * 60 * 60 * 1000, // 4 horas en milisegundos
+  //   signed: true
+  // })
 }
 
 export const getTokenFromCookies = (req) => {
